66b7b02b4a10bd0d8ac6ad8c94e9f4c956626d92
manifestonosuke <pierrenosuke@gmail.com>
2014-10-10 10:52:31 +0200

    v 0.9

--- loganalyse.py
+++ loganalyse.py
@@ -1,14 +1,18 @@
-#! /usr/bin/python2
+#!/usr/bin/python2
 
 import os 
+import sys
+import getopt
 
+PRGNAME=os.path.basename(sys.argv[0])
 
 count=0 
-limit=-1 
+option={}
 
 file="dovecot_error_log"
-fd=open(file)
-#refday="28/Sep/2014"
+option['file']=file
+option['count']=-1
+#refday="30/Sep/2014"
 refday=""
 
 if refday != "":
@@ -18,7 +22,50 @@ prevsec=-1
 this={}
 nb=0
 prevhms=-1
+PATTERN=('PUT','GET','DELETE','HEAD')
 
+def usage():
+        global PRGNAME
+        print PRGNAME,"usage"
+        print "debug sys args",sys.argv
+        #for arg in sys.argv:
+        #       print arg
+
+def parseargs(argv):
+        try:
+                opts, args = getopt.getopt(argv, "Ac:dhf:u:x", ["help", "url="])
+        except getopt.GetoptError:
+                usage()
+                sys.exit(2)
+        #print "Parsing opt",opts,"arg",args
+        global option
+        global DEBUG
+        for opt, arg in opts:
+                if opt in ("-h", "--help"):
+                        usage()
+                        sys.exit()
+                elif opt == '-A':
+                        option['ALL'] = 1
+                elif opt == '-c':
+                        option['count'] = arg
+                elif opt == '-d':
+                        DEBUG = 1
+                elif opt in "-f":
+                        option['file']=arg
+                        #print "using File "+file,
+                elif opt in "-x":
+                        option['cvs']=1
+
+if (len(sys.argv) != 1) :
+    parseargs(sys.argv[1:])
+
+
+if 'ALL' in option:
+    refday=""
+if 'cvs' in option:
+    print '{0:10s} {1:10s} {2:7s}{3:7s}{4:7s}{5:7s}'.format('date','hour','PUT','GET','DELETE','HEAD')
+
+fd=open(option['file'])
 while True:
     nb+=1
     l=fd.readline()
@@ -30,12 +77,13 @@ while True:
     count+=1
     if refday != "":
         displaydate=refday
-        if ldd != refday:
+        if ldd != refday: 
             print 'cont'+refday+":::"
-            continue
+        continue
     else:
-        displaydate=ldd
+       displaydate=ldd
     Q=lq.split()[0]
+    # this will contain the PUT/GET/DELETE ... 
     if Q in this.keys():
         this[Q]=this[Q]+1
         #print this
@@ -48,23 +96,30 @@ while True:
         if prevsec != s:
             string=""
             fstring=""
-            sorted={}
-            sorted=this
-            for k in sorted.keys():
-                #string=k+'='+str(sorted[k])+' '+string
-                fstring=string+" "+fstring
-            this={}
+            print '{0} {1} '.format(displaydate,prevhms),
+            for i in PATTERN:
+                if i in this.keys():
+                    if 'cvs' in option:
+                        print '{0} '.format(str(this[i])),
+                    else:
+                        print '{0}:{1} '.format(i,str(this[i])),
+                else:
+                    if 'cvs' in option:
+                        print '{0} '.format(0),
+                    else:
+                        print '{0}:{1} '.format(i,'0'),
+                this[i]=0
+            print
             #print '{0:20s} {1:10s} {2:7s}{3}'.format(refday,hms,Q,str(prevsec),string)
             #print '{0} {1} {2}'.format(displaydate,hms,string)
-            print '{0} {1} '.format(displaydate,prevhms),
-            for k in sorted.keys():
-                print '{0:10s} {1:10s}'.format(k,str(sorted[k])),
-            print
+            #print '{0} {1} '.format(displaydate,prevhms),
             prevsec=s
             prevhms=hms
         #else:
         #    print h,m,s 
-    if count == limit:
+    if count == int(option['count']):
+        print "Reaching limit lines parsed  "+option['count']+" lines"
         break
+    #else:
 
-exit(0)
+exit(2)
